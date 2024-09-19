5b3fb37fc6a030843e1ce42c00beb2425e95315c
Breaker22 <gastyg16@gmail.com>
2024-09-19 12:34:00 -0300

    Add init config

---
+++ src/main/java/ar/com/gbl/user/challenge/UserChallengeApplication.java
@@ -0,0 +1,13 @@
+package ar.com.gbl.user.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserChallengeApplication.class, args);
+	}
+
+}
